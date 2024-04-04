9ff65d09b83a4e88687756accffadaee247a5792
Mykola Lesiuk <100199767+Chitonosera@users.noreply.github.com>
2024-04-04 08:40:58 +0300

    end commit

--- app/src/main/java/com/example/advocate/DescActivity.kt
+++ app/src/main/java/com/example/advocate/DescActivity.kt
@@ -3,6 +3,7 @@ package com.example.advocate
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Adapter
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,20 +15,19 @@ class DescActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desc)
 
-
-
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewDesc)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        var adapter = DescAdapter(getDesc())
+        val adapter = DescAdapter(getDesc()) { desc ->
+            showDetailDialog(desc)
+        }
         recyclerView.adapter = adapter
 
         findViewById<Button>(R.id.btn_back).setOnClickListener {
-            startActivity(Intent(this, MenuActivity::class.java))
+            onBackPressed()
         }
-
     }
 
-    private fun showCaseDetailDialog(desc: Desc) {
+    private fun showDetailDialog(desc: Desc) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Детальна інформація")
 
@@ -49,9 +49,16 @@ class DescActivity : AppCompatActivity() {
 
     private fun getDesc(): MutableList<Desc> {
         return mutableListOf(
-            Desc("John 228", "Math"),
-            Desc("Jane Smith", "English"),
-            Desc("Michael Johnson", "Physics")
+            Desc("John Jones", "Сталася проблема з відсутністю платежів за оренду магазину, що може призвести до виписки з оренди."),
+            Desc("Jane Doe", "Клієнт став жертвою шахрайства через Інтернет під час покупки товарів у великих кількостях."),
+            Desc("Michael Johnson", "Проблеми з погашенням кредитів та відсутність можливості відновити платежі за ними."),
+            Desc("Alice Smith", "Потерпіла від крадіжки особистих даних, що призвело до фінансових втрат."),
+            Desc("Robert Brown", "Зіткнувся зі складнощами у вирішенні спору з сусідом щодо меж земельної ділянки."),
+            Desc("Emily Wilson", "Стала жертвою шахрайства з використанням крадених банківських карток."),
+            Desc("David Taylor", "Має проблеми з відшкодуванням збитків від автомобільної аварії через відсутність страховки."),
+            Desc("Sarah Anderson", "Потребує правової допомоги у вирішенні сімейних конфліктів та розлученні."),
+            Desc("Daniel Martinez", "Став жертвою домашнього насильства та потребує захисту від насильницького партнера."),
+            Desc("Olivia Garcia", "Має проблеми зі стягненням боргів від клієнтів після надання послуг.")
         )
     }
 }
--- app/src/main/java/com/example/advocate/DescAdapter.kt
+++ app/src/main/java/com/example/advocate/DescAdapter.kt
@@ -12,42 +12,34 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-class DescAdapter(private var descs: MutableList<Desc>) : RecyclerView.Adapter<DescAdapter.ViewHolder>() {
-    interface OnItemClickListener {
-        fun onItemClick(desc: Desc)
+class DescAdapter(private val descList: MutableList<Desc>, private val onItemClick: (Desc) -> Unit) : RecyclerView.Adapter<DescAdapter.DescViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DescViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.dialog_desc, parent, false)
+        return DescViewHolder(view)
     }
 
-    private var onItemClickListener: OnItemClickListener? = null
+    override fun onBindViewHolder(holder: DescViewHolder, position: Int) {
+        val desc = descList[position]
+        holder.bind(desc)
+    }
 
     override fun getItemCount(): Int {
-        return descs.size
+        return descList.size
     }
 
-    fun setOnItemClickListener(listener: OnItemClickListener) {
-        this.onItemClickListener = listener
-    }
+    inner class DescViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvDescName: TextView = itemView.findViewById(R.id.tvDescName)
+        private val tvShortDesc: TextView = itemView.findViewById(R.id.tvShortDesc)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.dialog_desc, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val desc = descs[position]
-        holder.bind(desc)
-
-        holder.itemView.setOnClickListener {
-            onItemClickListener?.onItemClick(desc)
-        }
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(desc: Desc) {
-            val tvDescName = itemView.findViewById<TextView>(R.id.tvDescName)
-            val tvShortDesc = itemView.findViewById<TextView>(R.id.tvShortDesc)
-
             tvDescName.text = desc.name
             tvShortDesc.text = desc.shortDesc
+
+            itemView.setOnClickListener {
+                onItemClick(desc)
+            }
         }
     }
 }
+
